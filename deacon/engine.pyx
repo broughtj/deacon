3f402242be7a0336097eed5b883cd197490bd559
peterjriser <31518624+peterjriser@users.noreply.github.com>
2017-11-01 17:13:27 -0600

    Update engine.pyx

--- deacon/engine.pyx
+++ deacon/engine.pyx
@@ -44,22 +44,28 @@ cdef class EuropeanBinomialEngine(BinomialEngine):
 cdef class AmericanBinomialEngine(BinomialEngine):
   """A concrete class for the American Binomial Option Pricing model."""
   cdef double calculate(self, Option option, MarketData data):
-      cdef double dt = option.expiry / self._nsteps
-      cdef double u = np.exp((data.rate - data.div) * dt + data.vol * np.sqrt(dt))
-      cdef double d = np.exp((data.rate - data.div) * dt - data.vol * np.sqrt(dt))
-      cdef double pu = (np.exp((data.rate - data.div) * dt) - d)/ (u - d)
-      cdef double pd = 1.0 - pu
-      cdef double disc = np.exp(-data.rate * dt)
-      cdef double dpu = disc * pu
-      cdef double dpd = disc * pd
-      cdef long numNodes = self._nsteps + 1
-
-      cdef double[:] spot_t = np.empty(numNodes, dtype=np.float64)
-      cdef double[:] call_t = np.empty(numNodes, dtype=np.float64)
-
-
-        
- 
+        cdef double dt = option.expiry / self._nsteps
+        cdef double u = np.exp((data.rate - data.div) * dt + data.vol * np.sqrt(dt))
+        cdef double d = np.exp((data.rate - data.div) * dt - data.vol * np.sqrt(dt))
+        cdef double pu = (np.exp((data.rate - data.div) * dt) - d)/ (u - d)
+        cdef double pd = 1.0 - pu
+        cdef double disc = np.exp(-data.rate * dt)
+        cdef double dpu = disc * pu
+        cdef double dpd = disc * pd
+        cdef long numNodes = self._nsteps + 1
+        cdef unsigned long i
+        cdef unsigned long j
+        cdef double[:] spot_t = np.empty(numNodes, dtype=np.float64)
+        cdef double[:] call_t = np.empty(numNodes, dtype=np.float64)
+        for i in range(numNodes):
+            spot_t[i] = S * (u ** (self._nsteps - i)) * (d ** (i))
+		    call_t[i] = option.payoff(spot_price[i])
+        for i in range((self._nsteps-1), -1, -1):
+		    for j in range(i+1):
+                call_t[j] = dpu * call_t[j] + dpd * call_t[j+1]
+			    spot_t[j] = spot_t[j] / u
+		        call_t[j] = np.maximum(call_t[j], option.payoff(spot_t[j]))
+            return call_t[0]
  
 
 
