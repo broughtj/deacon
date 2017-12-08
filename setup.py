39015c93cb5cda0256e203d2f547cb04dd73e3d5
Peter Riser <peterriser@Peters-MacBook-Pro.local>
2017-12-08 13:36:33 -0700

    Updating setup.py for C++14

--- setup.py
+++ setup.py
@@ -2,10 +2,17 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 import numpy as np
+#import os
+
+#os.environ["CC"] = "clang"
+#os.environ["CXX"] = "clang++"
+
+compile_args = ['-O3', '-g', '-std=c++14']
 
 extensions = [
     Extension("*", ["**/*.pyx"],
         include_dirs=[np.get_include(), "."],
+        extra_compile_args=compile_args,
         language="c++")
 ]
 
